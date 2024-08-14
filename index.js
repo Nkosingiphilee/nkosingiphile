a8b4a54a70fbfccbe5cba9e39d4629b6105bc8ee
Nkosingiphilee <phungula82@outlook.com>
2024-08-14 19:37:23 +0200

    added about me section

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-window.addEventListener(('onload'),()=>{
-    alert("loadind......................")
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-content");
+
+function opentab(tabname){
+    for(var tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+
+    for(var tabcontent of tabcontents){
+        tabcontents.classList.remove("active-tab");
+    }
+
 }
-)
