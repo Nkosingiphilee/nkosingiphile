58e6205fea52d5cb8c32fbb7520677d3bb55c389
nkosingiphilee <phungula82@outlook.com>
2024-08-23 22:03:16 +0300

    friday afternon contact javascripts

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+
+function submitMessage(event){
+    event.preventDefault()
+    const input_name=document.getElementById("name");
+    const input_email=document.getElementById("email");
+    const input_message=document.getElementById("Message");
+
+    const isNameValid=input_name.value !=='';
+    const isEmailValid=input_email.value !=='';
+    let isMessageValid=input_message.value !=='';
+
+    const isFormValid=isNameValid && isEmailValid && isMessageValid
+    alert(isFormValid)
+}
