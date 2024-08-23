fcd6a91aed7946e6d38989ff1f5035634970d6e4
nkosingiphilee <phungula82@outlook.com>
2024-08-23 22:13:10 +0300

    friday afternon contact javascripts

--- index.js
+++ index.js
@@ -22,10 +22,35 @@ function submitMessage(event){
     const input_email=document.getElementById("email");
     const input_message=document.getElementById("Message");
 
-    const isNameValid=input_name.value !=='';
-    const isEmailValid=input_email.value !=='';
-    let isMessageValid=input_message.value !=='';
+    const isNameValid=input_name.value.trim() !=='';
+    const isEmailValid=input_email.value.trim() !=='' && input_email.validity.valid;
+    let isMessageValid=input_message.value.trim() !=='';
 
     const isFormValid=isNameValid && isEmailValid && isMessageValid
     alert(isFormValid)
+
+    if (isFormValid) {
+        const formData = new FormData(event.target)
+        console.log(formData);
+        fetch('https://formspree.io/f/mayraako',
+            {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then(response => response.json())
+            .then(data => {
+                if (data.ok) {
+                    alert('Email successfully sent')
+                }
+            })
+
+
+    }else{
+        alert("is invalid")
+    }
+
 }
